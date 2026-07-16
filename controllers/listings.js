2f36746ffe960cfd7b1d6a352aa24b07a98e6ed1
Ayush Ubhad <ayushubhad2708@gmail.com>
2026-07-16 10:59:56 +0530

    48. implemented multer so that we can directly upload the files to our form. 49. installed dotenv package to store the the passwords and added the condition so tthat it can only be accessible when it is not in production. 50.created a cloudinary  acc , and saved the api key, key secret, and could name in the dotenv 51. installed cloudinary library

--- controllers/listings.js
+++ controllers/listings.js
@@ -26,6 +26,13 @@ if(!listing){
 module.exports.createListing = async (req,res,next)=>{    
       const newListing = new Listing(req.body.listing);  
       newListing.owner = req.user._id;
+      if (typeof req.file !== "undefined") {
+        let url ="/uploads/" + req.file.path; 
+        let filename = req.file.filename;
+        
+       
+        newListing.image = { url: url, filename: filename };
+    }
     await newListing.save();
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
--- routes/listing.js
+++ routes/listing.js
@@ -4,14 +4,17 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
-
+const multer = require("multer");
+const {storage} = require("../cloudConfig.js");
+const upload =multer({storage })
 //new route
 router.get("/new", isLoggedIn,listingController.renderNewForm)
 
 router.route("/")
 .get(wrapAsync(listingController.index))
-.post(validateListing,isLoggedIn,wrapAsync(listingController.createListing )
-);
+.post(isLoggedIn,upload.single("listing[image]"),validateListing,wrapAsync(listingController.createListing ))
+
+
 
 router.route("/:id")
 .get( wrapAsync(listingController.showListing))
